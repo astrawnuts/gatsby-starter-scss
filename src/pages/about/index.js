6d632ba747f4a2a6573bf22f760920162d5d121e
astrawnuts <astrawnuts@gmail.com>
2020-12-14 19:22:32 +0530

    tutorial two gatsby practice

---
+++ src/pages/about/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import "./about.scss"
+
+export default class about extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello from the about page in a paragraph</p>
+                hello from the about page
+            </div>
+        )
+    }
+}
---
+++ src/pages/aboutus.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import About from "./about/index"
+
+export default class aboutus extends Component, About {
+    render() {
+        return (
+            <About />
+        )
+    }
+}
+
